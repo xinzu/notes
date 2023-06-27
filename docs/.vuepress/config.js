06d149900d45b97ae894c3a13c8507850eae890a
yanhuan <yanhuan@uino.com>
2023-06-27 14:13:16 +0800

    feat: 新增目录

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,101 +6,105 @@
  * @LastEditTime : 2022-03-14 16:52:01
  */
 module.exports = {
-  title: 'yh的学习文档',
-  base: '/notes/',
-  dest: 'dist',
-  themeConfig: {
-    sidebarDepth: 4,
-    sidebar: [
-      {
-        title: '基础知识扩展',
-        children: [
-          {
-            title: '对象',
-            path: '/基础知识扩展/对象.md',
-          },
-          {
-            title: '正则',
-            path: '/基础知识扩展/正则.md',
-          },
-          {
-            title: '存储机制',
-            path: '/基础知识扩展/存储机制.md',
-          },
-          {
-            title: '禁用弹出框后退页面',
-            path: '/基础知识扩展/禁用弹出框后退页面.md',
-          },
-          {
-            title: '强制360浏览采用哪种内核',
-            path: '/基础知识扩展/强制360浏览采用哪种内核.md',
-          },
-          {
-            title: 'setTimeout',
-            path: '/基础知识扩展/setTimeout.md',
-          },
+    title: 'yh的学习文档',
+    base: '/notes/',
+    dest: 'dist',
+    themeConfig: {
+        sidebarDepth: 4,
+        sidebar: [
+            {
+                title: '基础知识扩展',
+                children: [
+                    {
+                        title: '对象',
+                        path: '/基础知识扩展/对象.md',
+                    },
+                    {
+                        title: '正则',
+                        path: '/基础知识扩展/正则.md',
+                    },
+                    {
+                        title: '存储机制',
+                        path: '/基础知识扩展/存储机制.md',
+                    },
+                    {
+                        title: '禁用弹出框后退页面',
+                        path: '/基础知识扩展/禁用弹出框后退页面.md',
+                    },
+                    {
+                        title: '强制360浏览采用哪种内核',
+                        path: '/基础知识扩展/强制360浏览采用哪种内核.md',
+                    },
+                    {
+                        title: 'setTimeout',
+                        path: '/基础知识扩展/setTimeout.md',
+                    },
+                    {
+                        title: '数组-单行代码',
+                        path: '/基础知识扩展/单行代码.md',
+                    },
+                ]
+            },
+            {
+                title: 'css',
+                children: [
+                    {
+                        title: 'css布局',
+                        path: '/css/布局.md',
+                    },
+                    {
+                        title: 'scss',
+                        path: '/css/scss、less.md',
+                    },
+                ]
+            },
+            {
+                title: 'http',
+                children: [{
+                    title: 'http状态码',
+                    path: '/http/http状态码.md',
+                }, {
+                    title: 'https',
+                    path: '/http/https.md',
+                }]
+            },
+            {
+                title: 'vue',
+                children: [
+                    {
+                        title: 'vue2',
+                        path: '/vue/vue.md'
+                    }, {
+                        title: 'vue2的一些高级用法',
+                        path: '/vue/Vue的一些高级用法.md',
+                    }, {
+                        title: 'vue3',
+                        path: '/vue/Vue3.md',
+                    }
+                ]
+            }, {
+                title: 'react',
+                children: [
+                    {
+                        title: 'react',
+                        path: '/react/react.md',
+                    }, {
+                        title: 'react-native',
+                        path: '/react/react-native.md'
+                    }
+                ]
+            }, {
+                title: 'typescript',
+                path: '/typescript/ts.md',
+            }, {
+                title: '工具类',
+                children: [
+                    {
+                        title: 'fiddler',
+                        path: '/工具类/fiddler/fiddler工具使用.md',
+                    }
+                ]
+            },
         ]
-      },
-      {
-        title: 'css',
-        children: [
-          {
-            title: 'css布局',
-            path: '/css/布局.md',
-          },
-          {
-            title: 'scss',
-            path: '/css/scss、less.md',
-          },
-        ]
-      },
-      {
-        title: 'http',
-        children: [{
-          title: 'http状态码',
-          path: '/http/http状态码.md',
-        }, {
-            title: 'https',
-            path: '/http/https.md',
-        }]
-      },
-      {
-        title: 'vue',
-        children: [
-          {
-            title: 'vue2',
-            path: '/vue/vue.md'
-          }, {
-            title: 'vue2的一些高级用法',
-            path: '/vue/Vue的一些高级用法.md',
-          }, {
-            title: 'vue3',
-            path: '/vue/Vue3.md',
-          }
-        ]
-      }, {
-        title: 'react',
-        children: [
-          {
-            title: 'react',
-            path: '/react/react.md',
-          }, {
-            title: 'react-native',
-            path: '/react/react-native.md'
-          }
-        ]
-      }, {
-        title: 'typescript',
-        path: '/typescript/ts.md',
-      }, {
-        title: '工具类',
-        children: [
-          {
-            title: 'fiddler',
-            path: '/工具类/fiddler/fiddler工具使用.md',
-          }
-        ]
-      },
-    ]
-  }
+    }
 }
