6d074cfae1bcefe9b1e6d98bab175cb2fafa3341
yanhuan <yanhuan@uino.com>
2023-09-13 16:30:35 +0800

    feat: react-diff算法

--- code/vue-demo/src/App.vue
+++ code/vue-demo/src/App.vue
@@ -1,10 +1,22 @@
 <template>
-    <router-view></router-view>
+    <div v-for="(item, index) in list" :key="index">
+        {{ index }}
+        <input type="text"  />
+    </div>
+    <!-- <router-view></router-view> -->
 </template>
 
 <script setup>
-
+import { ref, onMounted } from 'vue';
+const list = ref(['A', 'B', 'C', 'D', 'E', 'F']);
+onMounted(() => {
+    setTimeout(() => {
+        list.value.splice(3, 1);
+    }, 10000)
+})
 </script>
 <style lang='scss' scoped>
-
+input {
+    border: 1px solid black;
+}
 </style>
